6de5b624c3ee24f6b9662f8ed63e32060e94f331
Aakash Verma <4k45hr0ck5007@gmail.com>
2020-04-21 22:13:24 +0530

    added utility function for generating alphanumeric code

---
+++ app/utils.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+
+
+import string
+import random
+
+SHORTCODE_MIN  = getattr(settings, "SHORTCODE_MIN", 15)
+
+
+def code_generator(size=SHORTCODE_MIN, chars=string.ascii_lowercase+string.digits):
+    new_code = ""
+    return "".join(random.choice(chars) for _ in range(size))
+
+
+def create_shortcode(instance, size=SHORTCODE_MIN):
+    new_code = code_generator(size=size)
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(shortcode=new_code).exists()
+    if qs_exists:
+        return create_shortcode(size=size)
+    return new_code
